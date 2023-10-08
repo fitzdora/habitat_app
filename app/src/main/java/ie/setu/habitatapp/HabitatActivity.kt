d1453cdc4aadc81888f83c3b512eaff32e070b31
Isadora Fitzgerald <33418972+fitzdora@users.noreply.github.com>
2023-10-08 15:17:18 +0100

    Refactor Main and Layout

--- app/src/main/java/ie/setu/habitatapp/HabitatActivity.kt
+++ app/src/main/java/ie/setu/habitatapp/HabitatActivity.kt
@@ -3,9 +3,9 @@ package ie.setu.habitatapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class HabitatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_habitat)
     }
 }
