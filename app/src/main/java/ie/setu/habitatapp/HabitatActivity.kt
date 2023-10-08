57aebf9678b100195d5ba77e7a064bec2177d85a
Isadora Fitzgerald <33418972+fitzdora@users.noreply.github.com>
2023-10-08 16:50:34 +0100

    Setting colours and themes

--- app/src/main/java/ie/setu/habitatapp/HabitatActivity.kt
+++ app/src/main/java/ie/setu/habitatapp/HabitatActivity.kt
@@ -2,10 +2,16 @@ package ie.setu.habitatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
+import timber.log.Timber.Forest.i
 
 class HabitatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_habitat)
+
+        Timber.plant(Timber.DebugTree())
+
+        i("Habitat Activity Started...")
     }
 }
