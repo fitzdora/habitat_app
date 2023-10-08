19c128750a0b27bbca9db6d023cfc9d1ae3943fe
Isadora Fitzgerald <33418972+fitzdora@users.noreply.github.com>
2023-10-08 16:58:28 +0100

    Setting binding

--- app/src/main/java/ie/setu/habitatapp/HabitatActivity.kt
+++ app/src/main/java/ie/setu/habitatapp/HabitatActivity.kt
@@ -2,16 +2,26 @@ package ie.setu.habitatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ie.setu.habitatapp.databinding.ActivityHabitatBinding
 import timber.log.Timber
 import timber.log.Timber.Forest.i
 
 class HabitatActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHabitatBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_habitat)
 
+        binding = ActivityHabitatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         Timber.plant(Timber.DebugTree())
 
         i("Habitat Activity Started...")
+
+        binding.btnAdd.setOnClickListener(){
+            i("add Button Pressed")
+        }
     }
 }
