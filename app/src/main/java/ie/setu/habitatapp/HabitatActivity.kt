90ad101c41185b2b9d607ddd65668797768726c7
Isadora Fitzgerald <33418972+fitzdora@users.noreply.github.com>
2023-10-10 13:53:48 +0100

    Rejigged Habitat Type to common name of species

--- app/src/main/java/ie/setu/habitatapp/HabitatActivity.kt
+++ app/src/main/java/ie/setu/habitatapp/HabitatActivity.kt
@@ -2,6 +2,7 @@ package ie.setu.habitatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
 import ie.setu.habitatapp.databinding.ActivityHabitatBinding
 import timber.log.Timber
 import timber.log.Timber.Forest.i
@@ -21,7 +22,15 @@ class HabitatActivity : AppCompatActivity() {
         i("Habitat Activity Started...")
 
         binding.btnAdd.setOnClickListener(){
-            i("add Button Pressed")
+            val commonName = binding.commonName.text.toString()
+            if(commonName.isNotEmpty()){
+                i("add Button Pressed: $commonName")
+            }
+            else {
+                Snackbar
+                    .make(it, "Please Enter a Common Name", Snackbar.LENGTH_LONG)
+                    .show()
+            }
         }
     }
 }
