f6a38fa95aac77c0a3002f1d1f1ed97b8b105bb1
Isadora Fitzgerald <33418972+fitzdora@users.noreply.github.com>
2023-10-14 15:51:49 +0100

    Tidying list create and update

--- app/src/main/java/ie/setu/habitatapp/activities/activities/HabitatActivity.kt
+++ app/src/main/java/ie/setu/habitatapp/activities/activities/HabitatActivity.kt
@@ -1,6 +1,5 @@
 package ie.setu.habitatapp.activities.activities
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -10,7 +9,6 @@ import ie.setu.habitatapp.R
 import ie.setu.habitatapp.databinding.ActivityHabitatBinding
 import ie.setu.habitatapp.main.MainApp
 import ie.setu.habitatapp.models.HabitatModel
-import timber.log.Timber
 import timber.log.Timber.Forest.i
 
 class  HabitatActivity : AppCompatActivity() {
@@ -20,12 +18,15 @@ class  HabitatActivity : AppCompatActivity() {
     lateinit var app: MainApp
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var edit = false
         //setContentView(R.layout.activity_habitat)
 
         binding = ActivityHabitatBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
 
         binding.toolbarAdd.title = title
         setSupportActionBar(binding.toolbarAdd)
@@ -33,6 +34,8 @@ class  HabitatActivity : AppCompatActivity() {
         i("Habitat App Activity Started...")
 
         if(intent.hasExtra("species_edit")) {
+            edit = true
+            binding.btnAdd.setText(R.string.save_species)
             speciesType = intent.extras?.getParcelable("species_edit")!!
             binding.commonName.setText(speciesType.commonName)
             binding.speciesDescription.setText(speciesType.speciesDescription)
@@ -43,27 +46,30 @@ class  HabitatActivity : AppCompatActivity() {
             speciesType.commonName = binding.commonName.text.toString()
             speciesType.speciesDescription = binding.speciesDescription.text.toString()
             speciesType.habitatType = binding.habitatType.text.toString()
-            if(speciesType.commonName.isNotEmpty()) {
-                app.speciesTypes.create(speciesType.copy())
-                setResult(RESULT_OK)
-                finish()
-            }
-            else {
+            if(speciesType.commonName.isEmpty()) {
                 Snackbar
                     .make(it,R.string.enter_commonName, Snackbar.LENGTH_LONG)
                     .show()
             }
-        }
-    }
+            else {
+                if (edit) {
+                    app.speciesTypes.update(speciesType.copy())
+                } else {
+                    app.speciesTypes.create(speciesType.copy())
+                }
+            }
+                    setResult(RESULT_OK)
+                    finish()
+                }
+            }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
         return super.onCreateOptionsMenu(menu)
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
-            R.id.item_cancel -> {
-                finish()
-            }
+            R.id.item_cancel -> { finish() }
         }
         return super.onOptionsItemSelected(item)
     }
