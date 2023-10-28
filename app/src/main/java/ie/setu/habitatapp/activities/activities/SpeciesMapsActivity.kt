4d2c4778361ac306a3add5d1bde3d987f6e034da
Isadora Fitzgerald <33418972+fitzdora@users.noreply.github.com>
2023-10-28 12:32:40 +0100

    Getting Map for Species to Load

--- app/src/main/java/ie/setu/habitatapp/activities/activities/SpeciesMapsActivity.kt
+++ app/src/main/java/ie/setu/habitatapp/activities/activities/SpeciesMapsActivity.kt
@@ -2,19 +2,55 @@ package ie.setu.habitatapp.activities.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import ie.setu.habitatapp.databinding.ActivitySpeciesMapsBinding
+import ie.setu.habitatapp.databinding.ContentSpeciesMapsBinding
+import ie.setu.habitatapp.main.MainApp
 
 class SpeciesMapsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySpeciesMapsBinding
+    private lateinit var contentBinding: ContentSpeciesMapsBinding
+    lateinit var map: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivitySpeciesMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
 
+        contentBinding = ContentSpeciesMapsBinding.bind(binding.root)
+        contentBinding.mapView.onCreate(savedInstanceState)
+
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        contentBinding.mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        contentBinding.mapView.onLowMemory()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        contentBinding.mapView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        contentBinding.mapView.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        contentBinding.mapView.onSaveInstanceState(outState)
+    }
+
 }
